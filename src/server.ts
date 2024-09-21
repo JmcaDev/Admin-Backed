b66562aa70fcb090b56eff0c8a47b01d353075bb
JmcaDev <jmcavzla@gmail.com>
2024-09-21 12:33:44 -0400

    Configuracion DB

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express from "express"
+import router from "./routes"
+import db from "./config/db"
 
+//Conexion base de datos
+async function connectDB(){
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log("Conexion exitosa a la BD")
+    } catch (error) {
+        console.log(error)
+        console.log("Hubo un error al conectar a la DB")
+    }
+}
+
+connectDB()
 const server = express()
 
-//Routing
-server.get("/", (req,res) => {
-    res.send("Hola")
-})
+server.use("/api/products", router)
+
 
 export default server
